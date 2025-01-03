a2d73aca64a320de256e28b6eb2ba9658e08ba53
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-03 20:54:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,11 @@ with st.expander('**Data Frame**'):
   DF = pd.read_csv("https://raw.githubusercontent.com/RohanGJ/PMUMLAi/refs/heads/master/features1.csv")
   DF
   
-with st.expander('**INPUT**'):
-  st.write('Inputs to Model')
+with st.expander('**Error Inputs**'):
+  st.write('Provide Error Counts for each attributes')
   age    = st.selectbox('AGE of machine',list(range(0,19)))
+  E1C    = st.selectbox('Error1Count',[0,1,2])
+  E2C    = st.selectbox('Error2Count',[0,1,2])
+  E3C    = st.selectbox('Error3Count',[0,1,2])
+  E4C    = st.selectbox('Error4Count',[0,1,2])
+  E5C    = st.selectbox('Error5Count',[0,1,2])
