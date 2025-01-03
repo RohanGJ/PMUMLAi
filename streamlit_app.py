78e3203b82c165ed5860815a44eb999f8ed01b34
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-03 20:27:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('PREVENTIVE MAINTANANCE USING ML')
 
-st.write('Hello world!')
+st.info('This MODEL predicts wheather an equipment is about to fail in the near future.')
+
+with st.expander('**Data Frame**'):
+  st.write('Feature Dataset')
+  DF = pd.read_csv("https://drive.google.com/file/d/1KdbROD9dBgTfBvP1roISbnZ_JyAR9OQ5/view?usp=drive_link")
+  DF
