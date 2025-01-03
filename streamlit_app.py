acfad4c12098225260432f031965a33746709ab6
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-03 21:31:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,7 @@ import pandas as pd
 st.title('PREVENTIVE MAINTANANCE USING ML')
 
 st.info('This MODEL predicts wheather an equipment is about to fail in the near future.')
-
-with st.expander('**Data Frame**'):
-  st.write('Feature Dataset')
-  DF = pd.read_csv("https://raw.githubusercontent.com/RohanGJ/PMUMLAi/refs/heads/master/features1.csv")
-  DF
-  
+ 
 with st.expander('**Error Inputs**'):
   st.write('Provide Error Counts for each attributes')
   E1C    = st.selectbox('Error1Count',[0,1,2])
