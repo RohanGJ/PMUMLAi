5e900bdd71995c46a8e2446791135c513d1a5ae1
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-03 21:10:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,21 @@ with st.expander('**Error Inputs**'):
   E5C    = st.selectbox('Error5Count',[0,1,2])
   
 with st.expander('**Voltage Inputs**'):
-  st.write('Provide Mean and SD of Voltage')
+  st.write('Provide Mean and SD of VTin')
   Vmean  = st.slider('Mean Voltage',150.00,220.00,170.00)
-  Vsd    = st.slider('Standard Deviation Voltage',6.50,27.50,14.50)
+  Vsd    = st.slider('Standard Deviation (Voltage)',6.50,27.50,14.50)
+
+with st.expander('**Rotation Inputs**'):
+  st.write('Provide Mean and SD of RTin')
+  Rmean  = st.slider('Mean Rotation',260.00,500.00,450.00)
+  Rsd    = st.slider('Standard Deviation (Rotation)',19.00,105.00,50.00)
+
+with st.expander('** Pressure Inputs**'):
+  st.write('Provide Mean and SD of PRin')
+  Pmean  = st.slider('Mean Pressure',90.00,155.00,100.00)
+  Psd    = st.slider('Standard Deviation (Pressure)',4.00,29.00,9.50)
+
+with st.expander('**Vibration Inputs**'):
+  st.write('Provide Mean and SD of VBin')
+  VBmean  = st.slider('Mean Vibration',35.00,65.00,40.00)
+  VBsd    = st.slider('Standard Deviation (Vibration)',2.00,14.00,5.00)
