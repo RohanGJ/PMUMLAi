4c967a91dbb6dffea1c7a62653b6c30f65409def
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-03 23:09:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import requests 
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+LE = LabelEncoder()
+Scaler = StandardScaler()
+xtr = pd.read_csv("https://github.com/RohanGJ/PMUMLAi/raw/refs/heads/master/xtr.csv",header = None, sep = ',')
+Scaler.fit(xtr)
+
+def load_model_from_github(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure the request was successful
+    model = pickle.loads(response.content)
+    return model
+
+pickle_url = "https://github.com/RohanGJ/PMUMLAi/raw/refs/heads/master/RFCV1.pkl"
+
+model = load_model_from_github(pickle_url)
+
 st.title('PREVENTIVE MAINTANANCE USING ML')
 
 st.info('This MODEL predicts wheather an equipment is about to fail in the near future.')
@@ -35,14 +53,34 @@ with st.expander('**Vibration Inputs**'):
 
 MID    = st.slider('Machine ID',0,99,1)
 AGE    = st.slider('Age',0,20,1)
+MODEL  = st.selectbox('Model', [range(0,100)])
+FinLis = {
+    'machineID'    : MID,
+    'voltmean'     : Vmean,
+    'rotatemean'   : Rmean,
+    'pressuremean' : Pmean,
+    'vibrationmean': VBmean, 
+    'voltsd'       : Vsd,
+    'rotatesd'     : Rsd, 
+    'pressuresd'   : Psd, 
+    'vibrationsd'  : VBsd,
+    'error1count'  : E1C,
+    'error2count'  : E2C, 
+    'error3count'  : E3C, 
+    'error4count'  : E4C, 
+    'error5count'  : E5C,
+    'model'        : MODEL, 
+    'age'          : AGE, 
+}
+STDF = pd.DataFrame(FinLis)
 
-Model = open("RFCV1.pkl","rb")
-model = pickle.load(Model)
+STDF = Scaler.fit_transform(STDF)
 
-#FinLis = {
-#     
-#}
+st.subheader("USER INPUTS :")
+st.write(STDF)
 
-#DF = pd.DataFrame(FinLis)
+st.subheader("Prediction : ")
+st.write(model.predict(STDF))
 
-#model.predict()
+st.subheader("Prediction Probability :")
+st.write(model.predict_proba(STDF))
