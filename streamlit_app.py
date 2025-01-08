aa33fd02791d1f946a55aa347c2dbfaa5e1a334d
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-08 12:57:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,29 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import json
 import requests 
+from streamlit_lottie import st_lottie
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
+fail_logo_path = "D:/PMUI/fail1.json"
+pass_logo_path = "D:/PMUI/pass.json"
+pickle_url = "https://github.com/RohanGJ/PMUMLAi/raw/refs/heads/master/RFCV1.pkl"
+
+def load_lottiefile(filepath: str):
+  with open(filepath, "r") as f:
+    return json.load(f)
 
 def load_model_from_github(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    model = pickle.loads(response.content)
-    return model
-
-pickle_url = "https://github.com/RohanGJ/PMUMLAi/raw/refs/heads/master/RFCV1.pkl"
+  response = requests.get(url)
+  response.raise_for_status()
+  model = pickle.loads(response.content)
+  return model
 
 model = load_model_from_github(pickle_url)
 
-st.title('PREVENTIVE MAINTANANCE USING ML')
-
-st.info('This MODEL predicts wheather an equipment is about to fail in the near future.')
+st.title('MACHINE LEARNING')
+st.info("Preventine Maintanance")
 
 E1C,E2C,E3C,E4C,E5C = 0,0,0,0,0
 
@@ -25,7 +31,9 @@ Vmean,Rmean,Pmean,VBmean = 0.00,0.00,0.00,0.00
 
 Vsd,Rsd,Psd,VBsd = 0.00,0.00,0.00,0.00
 
-with st.expander('**Error Inputs**'):
+tab1,tab2,tab3,tab4,tab5,tab6 = st.tabs(["Error_count","Voltage","Rotations","Pressure","Vibration","MISC"])
+
+with tab1:
   st.write('Provide Error Counts for each attributes')
   E1C    = st.selectbox('Error1Count',[0,1,2])
   E2C    = st.selectbox('Error2Count',[0,1,2])
@@ -33,30 +41,30 @@ with st.expander('**Error Inputs**'):
   E4C    = st.selectbox('Error4Count',[0,1,2])
   E5C    = st.selectbox('Error5Count',[0,1,2])
   
-with st.expander('**Voltage Inputs**'):
+with tab2:
   st.write('Provide Mean and SD of VTin')
   Vmean  = st.slider('Mean Voltage',150.00,220.00,170.00)
   Vsd    = st.slider('Standard Deviation (Voltage)',6.50,27.50,14.50)
 
-with st.expander('**Rotation Inputs**'):
+with tab3:
   st.write('Provide Mean and SD of RTin')
   Rmean  = st.slider('Mean Rotation',260.00,500.00,450.00)
   Rsd    = st.slider('Standard Deviation (Rotation)',19.00,105.00,50.00)
 
-with st.expander('**Pressure Inputs**'):
+with tab4:
   st.write('Provide Mean and SD of PRin')
   Pmean  = st.slider('Mean Pressure',90.00,155.00,100.00)
   Psd    = st.slider('Standard Deviation (Pressure)',4.00,29.00,9.50)
 
-with st.expander('**Vibration Inputs**'):
+with tab5:
   st.write('Provide Mean and SD of VBin')
   VBmean  = st.slider('Mean Vibration',35.00,65.00,40.00)
   VBsd    = st.slider('Standard Deviation (Vibration)',2.00,14.00,5.00)
-
-MID    = st.slider('Machine ID',0,99,1)
-AGE    = st.slider('Age',0,20,1)
-modrang = list(range(0,100))
-MODEL  = st.selectbox('Model', modrang)
+with tab6:
+  MID    = st.slider('Machine ID',0,99,1)
+  AGE    = st.slider('Age',0,20,1)
+  modrang = list(range(0,100))
+  MODEL  = st.selectbox('Model', modrang)
 
 FinLis = {
       'machineID'    : [MID],
@@ -79,16 +87,43 @@ FinLis = {
 
 STDF = pd.DataFrame(FinLis)
 
-if st.button("See your Inputs"):
-  
-  st.subheader("USER INPUTS :")
-  st.write(STDF)
+def color_df(val):
+  if val > 0 and val < 100:
+    color = 'blue'
+  elif val > 99:
+    color = 'green'
+  elif val == 0:
+    color = 'red'
+  else:
+    color = 'orange'
+  return f'background-color: {color}'
 
+st.info("USER INPUTS :")
+st.dataframe(STDF.iloc[:,[0,1,2,3,4,5,6,15]].style.applymap(color_df))
+st.dataframe(STDF.iloc[:,7:-1].style.applymap(color_df))
+#st.write(STDF[:][:8])
+
+pred = []
 STDF = STDF.values
-if st.button("Run Model"):
+pred = list(model.predict(STDF))
 
-  st.subheader("Prediction : ")
-  st.write(model.predict(STDF))
+col1, col2 = st.columns(2, gap = "small")
 
-  st.subheader("Prediction Probability :")
-  st.write(model.predict_proba(STDF))
+col1.info("MODEL Eval")
+col1.subheader("Prediction : ")
+col1.write(model.predict(STDF))
+
+col1.subheader("Prediction Probability :")
+col1.write(model.predict_proba(STDF))
+  
+lottie_pass = load_lottiefile(pass_logo_path)
+lottie_fail = load_lottiefile(fail_logo_path)
+
+with col2:
+  st.info("STATUS")
+  if pred[0] == 0:
+    st_lottie(lottie_pass, speed = 0.75, loop = False)
+  elif pred[0] == 1:
+   st_lottie(lottie_fail, speed = 0.75, loop = True, reverse = True) 
+  else:
+    st.info("Press Run model after selecting inputs")
