09d635494d1f287608f8cb17479e3e2522020efd
RohanGJ <91448961+RohanGJ@users.noreply.github.com>
2025-01-10 11:58:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,10 +98,10 @@ def color_df(val):
   return f'background-color: {color}'
 
 st.info("USER INPUTS :")
-STDF.style.apply(color_df)
-st.dataframe(STDF)
-#st.dataframe(STDF.iloc[:,[0,1,2,3,4,5,6,15]].style.applymap(color_df))
-#st.dataframe(STDF.iloc[:,7:-1].style.applymap(color_df))
+#STDF.style.apply(color_df)
+#st.dataframe(STDF)
+st.dataframe(STDF.iloc[:,[0,1,2,3,4,5,6,15]].style.applymap(color_df))
+st.dataframe(STDF.iloc[:,7:-1].style.applymap(color_df))
 #st.write(STDF[:][:8])
 
 pred = []
