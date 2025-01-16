12d7c7ebab4e9e731822b3d05bc44174b220fb4e
Rohan George John <91448961+RohanGJ@users.noreply.github.com>
2025-01-16 21:03:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def load_model_from_github(url):
 
 model = load_model_from_github(pickle_url)
 
-st.title('MACHINE LEARNING')
+st.title('LUMINAR')
 st.info("Preventine Maintanance")
 
 E1C,E2C,E3C,E4C,E5C = 0,0,0,0,0
