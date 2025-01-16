f45e1bffc75ddec4060f27ad7403707a2a55ab54
Rohan George John <91448961+RohanGJ@users.noreply.github.com>
2025-01-16 21:04:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def load_model_from_github(url):
 
 model = load_model_from_github(pickle_url)
 
-st.title('LUMINAR')
+st.title('Machine wear and tear prediction')
 st.info("Preventine Maintanance")
 
 E1C,E2C,E3C,E4C,E5C = 0,0,0,0,0
